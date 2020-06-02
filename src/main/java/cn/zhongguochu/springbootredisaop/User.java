12c12a44ad549ce74430aadb99b017be728b8110
w_w <1146654010@qq.com>
2020-06-02 21:13:56 +0800

    jedis的使用

---
+++ src/main/java/cn/zhongguochu/springbootredisaop/User.java
@@ -0,0 +1,63 @@
+package cn.zhongguochu.springbootredisaop;
+
+public class User {
+    private String id;
+
+    private String name;
+
+    private Integer age;
+
+    private String phone;
+
+    private Integer sex;
+
+    public User(){}
+
+    public User(String id, String name, Integer age, String phone, Integer sex) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.sex = sex;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+}
--- src/main/java/cn/zhongguochu/springbootredisaop/redis/RedisServiceImpl.java
+++ src/main/java/cn/zhongguochu/springbootredisaop/redis/RedisServiceImpl.java
@@ -1,4 +1,43 @@
 package cn.zhongguochu.springbootredisaop.redis;
 
-public class RedisServiceImpl {
+import cn.zhongguochu.springbootredisaop.User;
+import com.alibaba.fastjson.JSON;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public class RedisServiceImpl implements RedisService{
+    public void set(String key, String value) {
+
+    }
+
+    public String get(String key) {
+        return null;
+    }
+
+    public void hset(String key, String mapKey, Object mapValue) {
+
+    }
+
+    public <T> T hget(String key, String mapKey, Class<T> t) {
+        return null;
+    }
+
+    public void hmset(String key, String... params) {
+
+    }
+
+    public static void main(String[] args) {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxIdle(8);
+        config.setMaxTotal(18);
+        JedisPool jedisPool = new JedisPool(config, "49.233.168.202", 6379, 2000, "yMEKq5IzoAUyqBl1");
+        Jedis jedis = jedisPool.getResource();
+        User user = new User("1", "张三", 12, "1954654632", 1);
+        jedis.set("key", JSON.toJSONString(user));
+        String value = jedis.get("key");
+        System.out.println(value);
+        User user1 = (User)JSON.parse(value);
+        System.out.println(user1);
+    }
 }
